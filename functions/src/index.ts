a76ec41acee06c97cde938d91cafb8fef2ce067a
tylerm390 <77948992+tylerm390@users.noreply.github.com>
2023-11-08 02:18:55 -0500

    Added Login and Register API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
-import * as functions from 'firebase-functions'
-import * as admin from 'firebase-admin'
+import { https } from 'firebase-functions'
+import admin from 'firebase-admin'
 
 import express, { Express, Request, Response } from 'express'
 const app: Express = express()
@@ -40,15 +40,64 @@ app.use(
     origin: allowedOriginsList,
     methods: allowedMethodsList,
     allowedHeaders: allowedHeadersList,
-  }),
+  })
 )
 
-import * as firebase from 'firebase/app'
-firebase.initializeApp(firebaseConfig)
+import { initializeApp } from 'firebase/app'
+initializeApp(firebaseConfig)
 
+// Test API
 app.post('/helloWorld', (req: Request, res: Response) => {
   console.log('Hello!')
   res.json({ str: 'Hello World!' })
 })
 
-exports.api = functions.https.onRequest(app)
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth'
+
+const auth = getAuth()
+
+// Login user with email
+// Expects input containing email and password
+app.post('/loginWithEmail', (req: Request, res: Response) => {
+  const user = req.body
+  signInWithEmailAndPassword(auth, user.email, user.password)
+    .then((data) => {
+      return res.status(200).json({ userID: data.user.uid })
+    })
+    .catch((err) => {
+      if (err.code === 'auth/invalid-login-credentials')
+        return res.status(401).json({ general: 'Invalid Credentials' })
+      else if (err.code === 'auth/invalid-email')
+        return res.status(401).json({ general: 'Invalid Email' })
+      else if (err.code === 'auth/missing-email')
+        return res.status(401).json({ general: 'Missing Email' })
+      else if (err.code === 'auth/missing-password')
+        return res.status(401).json({ general: 'Missing Password' })
+      else return res.status(500).json({ error: err.code })
+    })
+})
+
+app.post('/registerWithEmail', (req: Request, res: Response) => {
+  const newUser = req.body
+  createUserWithEmailAndPassword(auth, newUser.email, newUser.password)
+    .then((data) => {
+      return res.status(201).json({ general: 'User Created' })
+    })
+    .catch((err) => {
+      if (err.code === 'auth/email-already-in-use')
+        return res.status(401).json({ general: 'Email in Use' })
+      else if (err.code === 'auth/invalid-email')
+        return res.status(401).json({ general: 'Invalid Email' })
+      else if (err.code === 'auth/missing-email')
+        return res.status(401).json({ general: 'Missing Email' })
+      else if (err.code === 'auth/missing-password')
+        return res.status(401).json({ general: 'Missing Password' })
+      else return res.status(500).json({ error: err.code })
+    })
+})
+
+exports.api = https.onRequest(app)
