4e4abfef14ac692bed8611c8580f9c2302c71bbd
Cameron Custer <ccuster@rapidcity.ptl.ucf.edu>
2024-02-27 15:52:32 -0500

    fix updateProblems API to await promises

--- functions/src/updateProblems.ts
+++ functions/src/updateProblems.ts
@@ -3,12 +3,14 @@ import { Request, Response } from 'express'
 import { doc, updateDoc } from 'firebase/firestore'
 import { db } from './util'
 
-export function updateProblems(req: Request, res: Response) {
+export async function updateProblems(req: Request, res: Response) {
   try {
     const problems: Problem[] = req.body.problems
-    problems.forEach((problem) => {
-      updateDoc(doc(db, 'Problems', problem.problemID), problem)
-    })
+    await Promise.all(
+      problems.map((problem) =>
+        updateDoc(doc(db, 'Problems', problem.problemID), problem),
+      ),
+    )
   } catch (err) {
     return res.status(500).json({ error: 'Failed to update problem' })
   }
